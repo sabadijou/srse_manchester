791f12a6b0b0739e748888a743d653a04751bd81
Sadegh Abadijou <80114254+sabadijou@users.noreply.github.com>
2024-01-24 01:51:00 +0000

    Upgrade release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='srse_manchester',
-    version='0.0.2',
+    version='0.0.4',
     packages=find_packages(),
     description='My Ontology Lookup Service',
     long_description=open('README.md').read(),

891b71b3de1f756e6d5530668e7977cb87d30659
Sadegh Abadijou <s.abadijou@gmail.com>
2024-01-24 11:24:40 +0000

    Refactored: Make the app executable

--- explorer.py
+++ explorer.py
@@ -1,24 +1,22 @@
 from srse_manchester.srse_manchester import explore_ontology
-import sys
-import argparse
 import logging
 
-if __name__ == '__main__':
 
+def main():
     logging.basicConfig(level=logging.ERROR)
 
-    parser = argparse.ArgumentParser(description='Ontology Lookup Service')
+    while True:
+        ontology_id = input("Enter ontology ID (or 'exit' to quit): ").strip()
 
-    parser.add_argument('--ontology_id', type=str, help='The ID of the ontology to fetch data from')
+        if ontology_id.lower() == 'exit':
+            print("Exiting program.")
+            break
 
-    if len(sys.argv) == 1:
-        parser.print_help(sys.stderr)
-        sys.exit(1)
+        ontology = explore_ontology(ontology_id)
 
-    args = parser.parse_args()
+        if ontology is not None:
+            ontology.print_content()
 
-    ontology = explore_ontology(args.ontology_id)
 
-    if ontology is not None:
-        ontology.print_content()
-
+if __name__ == '__main__':
+    main()
